9f8322416a9063716c5104a1b7dafffd096580d7
Eason Zhang <yishen.zhang@kuleuven.be>
2022-07-23 22:03:37 +0200

    update float method

--- pdfToTable.py
+++ pdfToTable.py
@@ -4,6 +4,21 @@ import pandas as pd
 import io, os
 os.chdir(os.getcwd())
 
+def colTofloat(df_col):
+    """
+    convert dataframe from string to float and escape TypeError from type like float
+    """
+    col_array = []
+    for idx, ele in enumerate(df_col):
+    
+        try: 
+            col_array.append(float(ele))
+        except:
+            col_array.append(ele)
+
+    return col_array
+
+# read your desire page of the table
 target_page = 12
 
 file = read_pdf('yourpdfname.pdf',pages = target_page,
@@ -12,8 +27,9 @@ table = tabulate(file)
 
 df_table = pd.read_fwf(io.StringIO(table))
 
+# seperate dataframe by multi whitespace, convert string to float for data
 for col in df_table.columns:
-    split_col = df_table[col].str.split(" ", n = 1, expand = True)[1]
-    df_table[col] = split_col
-    
+    split_col = df_table[col].str.split(" +", n = 1, expand = True)[1]
+    df_table[col] = colTofloat(split_col)
+
 df_table.to_excel("table.xlsx")
